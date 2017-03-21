db593aa762f465dac341fb3339c4274538b343e0
Gary Greene <greeneg@tolharadys.net>
2017-03-21 00:13:00 -0400

    Expand API output for the API root

--- lib/Classifier/REST.pm
+++ lib/Classifier/REST.pm
@@ -17,10 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License. 
 #
-# $Rev: $
-# $Author: $
-# $Date: $
-#
 
 package Classifier::REST;
 
@@ -30,6 +26,7 @@ use English;
 use utf8;
 
 use Config::IniFiles;
+use Date::Format;
 use Dancer2;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
@@ -71,23 +68,53 @@ sub get_root {
     my %config = %{$_c};
     undef $_c;
 
-    use Data::Dumper;
+    my @lt = localtime(time);
+    my $jf_date     = strftime('%Y-%m-%dT%H:%M:%S.000Z', @lt);
 
-    print STDERR Dumper(%config), "\n";
-    
     status 200;
     my $root = {
-        'data'  => undef,
-        'links' => {
-            'self' => $config{webroot} . "/"
+        'data'       => undef,
+        'jsonapi'    => { 'version' => '1.0' },
+        'links'      => {
+            'self'             => $config{webroot} . "/api/v1/",
+            'computers'        => $config{webroot} . "/api/v1/computers",
+            'classes'          => $config{webroot} . "/api/v1/classes",
+            'distributions'    => $config{webroot} . "/api/v1/distributions",
+            'environments'     => $config{webroot} . "/api/v1/environments",
+            'operatingsystems' => $config{webroot} . "/api/v1/operatingsystems",
+            'parent'           => $config{webroot} . "/api/v1/",
+        },
+        'attributes' => {
+            'created'          => $jf_date
         }
     };
     return $root;
 }
 
+sub get_computers {
+    my $sub = (caller(0))[3];
+    err_log("== DEBUGGING ==: In function: $sub") if $DEBUG;
+    my $_c  = shift;
+    my %config = %{$_c};
+    undef $_c;
+
+    
+
+    status 200;
+    my $computers = {
+        'data'    => undef,
+        'jsonapi' => { 'version' => '1.0' },
+        'links'   => {
+            'self'   => $config{webroot} . "/api/v1/computers",
+            'parent' => $config{webroot} . "/api/v1/",
+        }
+    };
+    return $computers;
+}
+
 sub main {
     err_log(">> Starting the Puppet Classifier API server version ". $Classifier::Constants::version);
-    err_log("\n-------------------------------------------------------------");
+    err_log("-------------------------------------------------------------");
 
     # Dancer2 configuration
     my $appdir = config->{appdir};
@@ -96,6 +123,9 @@ sub main {
 
     set serializer     => 'JSON';
     set startup_info   => 1;
+
+    use Data::Dumper;
+    print STDERR "== DEBUGGING ==:\n", Dumper(config), "\n" if $DEBUG;
 
     # our get URLs
     get '/'                              => sub { get_root( \%config ) };
